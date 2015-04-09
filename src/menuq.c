1bfe85247f2629b3609a4410ce5e3ae82c33437f
calint <calin.tenitchi@gmail.com>
2015-04-09 15:24:12 -0500

    .

--- src/menuq.c
+++ src/menuq.c
@@ -1,3 +1,4 @@
+#define APP "menuq"
 #include<X11/Xlib.h>
 #include<stdio.h>
 #include<stdlib.h>
@@ -6,39 +7,57 @@
 #include<X11/keysym.h>
 #include <X11/Xutil.h>
 int main(){
-	puts("menuq");
+	puts(APP);
 	Display*dpy=XOpenDisplay(NULL);
 	if(!dpy){
 		//fprintf(stderr, "!!! could not open display\n");
 		return 1;
 	}
-	int scr=DefaultScreen(dpy);
-	unsigned int scr_w=DisplayWidth(dpy,scr);
-	Window win=XCreateSimpleWindow(dpy,RootWindow(dpy,scr),0,0,scr_w,23,0,BlackPixel(dpy,scr),BlackPixel(dpy,scr));
+	const int scr=DefaultScreen(dpy);
+	const int screen_width=DisplayWidth(dpy,scr);
+	const int win_height=23;
+	const Window win=XCreateSimpleWindow(dpy,RootWindow(dpy,scr),0,0,screen_width,win_height,0,BlackPixel(dpy,scr),BlackPixel(dpy,scr));
+//	XSelectInput(dpy,win,ExposureMask|KeyPressMask);
 	XSelectInput(dpy,win,KeyPressMask);
 	XMapWindow(dpy,win);
-	GC gc=XCreateGC(dpy,win,0,NULL);
+	const GC gc=XCreateGC(dpy,win,0,NULL);
+	const int x_init=(screen_width>>1)-(screen_width>>2)+(screen_width>>3);
+	int x=x_init;
+	const int y_init=(win_height>>1)+(win_height>>2);
+	int y=y_init;
+	const char cursor_str[1]="_";
 	XSetForeground(dpy,gc,WhitePixel(dpy,scr));
-	int x=(scr_w>>1)-(scr_w>>2)+(scr_w>>3);
-	int y=(23>>1)+(23>>2);
-	XDrawString(dpy,win,gc,x,y,"_",1);
-	int keycode=0;
-//	char*str;
-	const int buflen=32;
-	char buf[buflen];
+	XDrawString(dpy,win,gc,x,y,cursor_str,1);
+	char buf[32];
 	int bufi=0;
 	char*bufp=buf;
+	*bufp=0;
+	const int char_width=7;
+	const int char_y_wiggle=3;
+	const int char_y_wiggle_up=-1;
+	const int options_start_x=640;
 	int go=1;
+	XEvent e;
 	while(go){
-		XEvent e;
 		XNextEvent(dpy,&e);
 		switch(e.type){
 		default:
 			break;
+		case Expose:
+//			XSetForeground(dpy,gc,BlackPixel(dpy,scr));
+//			XFillRectangle(dpy,win,gc,0,0,screen_width,win_height);
+//			XSetForeground(dpy,gc,WhitePixel(dpy,scr));
+//			const int buflen=strlen(buf);
+//			printf("%d   [%s]\n",buflen,buf);
+//			XDrawString(dpy,win,gc,x_init,y_init,buf,buflen);
+//			x=x_init+char_width*buflen;
+//			XDrawString(dpy,win,gc,x,y,cursor_str,1);
+			break;
 		case DestroyNotify:
 			go=0;
 			break;
-		case KeyPress:
+		case KeyPress:{
+			int keycode=0;
 			//printf("KeyPress %d %d\n",e.xbutton.button,e.xbutton.state);
 			keycode=e.xkey.keycode;
 			if(keycode==9){//esc
@@ -56,43 +75,41 @@ int main(){
 			if(keycode==22){//backspace
 				if(bufi==0)
 					break;
-				x-=7;
+				x-=char_width;
 				XSetForeground(dpy,gc,BlackPixel(dpy,scr));
-				XFillRectangle(dpy,win,gc,x,0,14,23);
+				XFillRectangle(dpy,win,gc,x,0,char_width<<1,win_height);
 				XSetForeground(dpy,gc,WhitePixel(dpy,scr));
 				XDrawString(dpy,win,gc,x,y,"_",1);
 				bufi--;
 				bufp--;
 				*bufp=0;
 			}else{
+				// clear cursor
 				XSetForeground(dpy,gc,BlackPixel(dpy,scr));
-				XFillRectangle(dpy,win,gc,x,0,7,23);
+				XFillRectangle(dpy,win,gc,x,0,char_width,win_height);
 				XSetForeground(dpy,gc,WhitePixel(dpy,scr));
-
-				char buffer[20];
+				// get printable character
+				char buffer[4];
 				KeySym keysym;
-				//XComposeStatus compose;
 				XLookupString(&e.xkey,buffer,sizeof buffer,&keysym,NULL);
-				//str=XKeysymToString(XKeycodeToKeysym(dpy,keycode,0));
-				//if(keycode==65){//space
-				//	str=" ";
-				//}else if(keycode==61){
-				//	str="-";
-				//}
+				if(!buffer[0])// printable character ?
+					break;
 				XDrawString(dpy,win,gc,x,y,buffer,1);
-				x+=7;
+				x+=char_width;
 				*bufp++=buffer[0];
 				bufi++;
-				if(bufi>buflen)
+				if((bufi+1)>=(int)sizeof buf)// buffer full ?
 					go=0;
-				y+=-1+rand()%3;
-				XDrawString(dpy,win,gc,x,y,"_",1);
+				y+=char_y_wiggle_up+rand()%char_y_wiggle;
+				XDrawString(dpy,win,gc,x,y,cursor_str,1);
 			}
-			XFillRectangle(dpy,win,gc,640,0,800,22);
+			XFillRectangle(dpy,win,gc,options_start_x,0,screen_width-options_start_x-1,win_height-1);
+			break;
+			}
 		}
 	}
 	XCloseDisplay(dpy);
-//	puts(buf);
+	if(!*buf)return 0;//empty string
 	strcat(buf,"&");
 	return system(buf);
 }
